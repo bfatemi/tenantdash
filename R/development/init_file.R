73b68de573b54f9612474750d1f399b4e80ceade
BobbyFatemi <bfatemi07@gmail.com>
2017-08-05 16:36:32 -0700

    init files added (pre-build)

--- R/development/init_file.R
+++ R/development/init_file.R
@@ -60,12 +60,42 @@ use_priv_dir(pkg_dir)
 devtools::use_testthat(pkg_dir)
 devtools::use_github(pkg = pkg_dir)
 
-trav_txt <- capture.output(devtools::use_travis(pkg = pkg_dir), type = "message")
-trav_link <- trav_txt[length(trav_txt)]
-
+##
+## Add readme file and store path and contents for updating
+##
 try(devtools::use_readme_md(devtools::as.package(pkg_dir)), silent = TRUE)
 
 rm_path <- paste0(pkg_dir, "/README.md")
 rm_txt <- readLines(rm_path)
+
+
+##
+## Add travis file and update readme contents
+##
+trav_txt <- capture.output(devtools::use_travis(pkg = pkg_dir), type = "message")
+trav_link <- trav_txt[length(trav_txt)]
+
 rm_txt[1] <- paste0(rm_txt[1], " ", trav_link)
+
+
+##
+## Add coverage and update readme contents
+##
+cov_txt <- capture.output(devtools::use_coverage(pkg = pkg_dir), type = "message")
+
+# parse cov link for readme and hook for travis file
+cov_link <- cov_txt[which(stringr::str_detect(cov_txt, "\\[\\!\\[Coverage"))]
+cov_hook <- paste0(paste0(cov_txt[which(stringr::str_detect(cov_txt, "after_success")):length(cov_txt)], collapse = "\n"), "\n")
+
+rm_txt[1] <- paste0(rm_txt[1], " ", cov_link)
+
+# update travis file with coverage hook
+cat(cov_hook, file = ".travis.yml", append = TRUE)
+
+##
+## add cran badge and store final updated readme
+##
+cran_txt <- capture.output(devtools::use_cran_badge(pkg = pkg_dir), type = "message")
+cran_link <- cran_txt[length(cran_txt)]
+rm_txt[1] <- paste0(rm_txt[1], " ", cran_txt)
 writeLines(paste0(rm_txt, collapse = "\n"), rm_path)
---
+++ README.md
@@ -0,0 +1,20 @@
+# tenantdash [![Travis-CI Build Status](https://travis-ci.org/bfatemi/tenantdash.svg?branch=master)](https://travis-ci.org/bfatemi/tenantdash) [![Coverage Status](https://img.shields.io/codecov/c/github/bfatemi/tenantdash/master.svg)](https://codecov.io/github/bfatemi/tenantdash?branch=master) [![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/tenantdash)](https://cran.r-project.org/package=tenantdash)
+
+The goal of tenantdash is to ...
+
+## Installation
+
+You can install tenantdash from github with:
+
+```R
+# install.packages("devtools")
+devtools::install_github("tenantdash/bfatemi")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+```R
+...
+```
